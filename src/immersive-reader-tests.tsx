deda2dbf4e8dd72b9016d59f2db586d79b4daaf5
Aleksey <aleksin@dtml.org>
2020-09-11 21:57:34 -0700

    Cleaning up variables

--- src/immersive-reader-tests.tsx
+++ src/immersive-reader-tests.tsx
@@ -1,11 +1,13 @@
 import * as React from 'react'
 import { shallow } from 'enzyme'
+import { expect } from 'chai';
+import 'mocha';
 
 import ImmersiveReader from './immersive-reader';
 
 describe('<ImmersiveReader />', () => {
   it('should render', () => {
     const wrapper = shallow(<ImmersiveReader title="TestTitle" text="Test Text" locale="en-us" tokenURL="" />)
-    expect(wrapper).toBeDefined()
+    expect(wrapper).to.equal("");
   })
 })
